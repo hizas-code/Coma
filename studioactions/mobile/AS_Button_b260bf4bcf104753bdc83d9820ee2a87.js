b1b676c1bbcafeebdd6a4e25269c313a936ed698
hasobiroid <roid.siboy@yahoo.co.id>
2017-09-21 21:56:41 +0700

    21.56, 21 September, register page has been created

---
+++ studioactions/mobile/AS_Button_b260bf4bcf104753bdc83d9820ee2a87.js
@@ -0,0 +1,3 @@
+function AS_Button_b260bf4bcf104753bdc83d9820ee2a87(eventobject) {
+    HomeForm.show();
+}
---
+++ studioactions/mobile/AS_Label_c3b90d08d0fb43f9bb9e3817c2b39d87.js
@@ -0,0 +1,3 @@
+function AS_Label_c3b90d08d0fb43f9bb9e3817c2b39d87(eventobject, x, y) {
+    RegisterForm.show();
+}
