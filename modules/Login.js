7023bcec1e467143fc74193eed73af8bee8fe7ac
hasobiroid <roid.siboy@yahoo.co.id>
2017-09-21 19:08:40 +0700

    19.08, 21 September

---
+++ modules/Login.js
@@ -0,0 +1,19 @@
+/* Creating a component's Object */
+var login = new component.ui.login({
+                "clipBounds": true,
+                "height": "100%",
+                "id": "login",
+                "isVisible": true,
+                "left": "0dp",
+                "skin": "CopyslFbox0i25b6c13d6274d",
+                "top": "0dp",
+                "width": "100%",
+                "zIndex": 1
+            }, {}, {});
+/* Setting component's Pass Through and Custom properties */
+            login.logoSource = "logo.png";
+            login.usernamePlaceholder = "Username";
+           login.facebookLogin = true;
+            login.linkedInLogin = true;            
+/*Adding the Component to the Form*/
+            this.view.add(login);
